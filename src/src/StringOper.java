e117ff4ed8796e21291dc30051f23d163605ea48
surabo2580 <dassuraj980@gmail.com>
2022-08-09 12:35:56 +0530

    str operation

---
+++ src/src/StringOper.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StringOper {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first string");
+        String str = sc.nextLine();
+        System.out.println("enter second string");
+        String str2 = sc.nextLine();
+
+
+        StringBuilder input = new StringBuilder();
+
+        input.append(str);
+        input.append(str2);
+        input.reverse();
+        System.out.println(input);
+        System.out.println("length is "+ str.length());
+        System.out.println(str + str2);
+        if(str == str2){
+            System.out.println("equal");
+        }
+
+
+    }
+}
+
+
