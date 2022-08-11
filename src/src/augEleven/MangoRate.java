6a9d7a1d9ce637b6409c5cc35d396944308c32bf
surabo2580 <dassuraj980@gmail.com>
2022-08-11 17:27:48 +0530

    mango rate solution pushed

--- src/src/augEleven/MangoRate.java
+++ src/src/augEleven/MangoRate.java
@@ -2,17 +2,27 @@ package augEleven;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class MangoRate {
     public static void main(String args[]){
         HashMap<String,Integer> mangorate = new HashMap<>();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter quantity");
+        int quantity = sc.nextInt();
         mangorate.put("mango1",100);
         mangorate.put("mango2",200);
         mangorate.put("mango3",300);
 
+
         for(Map.Entry<String,Integer> iterator : mangorate.entrySet()){
+
             System.out.println("price of the " + iterator.getKey() + " is " + iterator.getValue() + " per kg ");
+            int price = iterator.getValue() * quantity;
+            System.out.println("total price of the " + iterator.getKey() + " is " + price);
         }
+
+
     }
 }
